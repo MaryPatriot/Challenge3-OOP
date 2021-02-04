819322a3c6851b6f01e11975b0bd14494f714c1b
Mary <mary.vatanparast@gmail.com>
2021-02-04 10:57:41 +0100

    Add project files.

---
+++ Challenge3 OOP/BMI.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge3_OOP
+{
+    class BMI
+    {
+        public string FullName;
+        public double Mass;
+        public double Height;
+        public double BMIJohn;
+        public double BMIMark;
+
+
+        public BMI(string FullName, double Mass, double Height)
+        {
+            this.FullName = FullName;
+            this.Mass = Mass;
+            this.Height = Height;
+
+        }
+
+        public string CalculateBMIJohn()
+        {
+            double BMIJohn = Mass / (Math.Pow(Height, 2));
+            return $"John :Your BMI is: {BMIJohn}";
+        }
+
+        public string CalculateBMIMark()
+        {
+            double BMIMark = Mass / (Math.Pow(Height, 2));
+            return $"Mark :Your BMI is: {BMIMark}";
+        }
+
+        public string BMICom()
+        {
+            if (BMIMark > BMIJohn)
+            {
+
+                return $"Mark has higher BMI than John";
+
+            }
+            else if (BMIMark < BMIJohn)
+            {
+                return $"John has higher BMI than Mark";
+            }
+
+            else if (BMIMark == BMIJohn)
+            {
+                return $"John and Mark have the same BMI";
+            }
+
+            else
+            {
+                return "Error";
+            }
+
+
+
+        }
+    }
+
+
+ }
+
---
+++ Challenge3 OOP/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge3_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BMI  Mark = new BMI("Mark Kristofer", 152, 95);
+            BMI  John = new BMI("John Robert", 192, 50);
+
+            Console.WriteLine(John.FullName);
+            Console.WriteLine(John.Mass);
+            Console.WriteLine(John.Height);
+
+            Console.WriteLine("****************************************************");
+            Console.WriteLine(Mark.FullName);
+            Console.WriteLine(Mark.Mass);
+            Console.WriteLine(Mark.Height);
+
+            Console.WriteLine("****************************************************");
+
+
+            Console.WriteLine(John.CalculateBMIJohn());
+            Console.WriteLine(Mark.CalculateBMIMark());
+            Console.WriteLine(John.BMICom());
+            Console.WriteLine(Mark.BMICom());
+
+
+            
+        }
+    }
+}
